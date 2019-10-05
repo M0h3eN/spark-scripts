c1e8308c1784178ed1f5434b2bfccd3c80b185fc
Mohsen Hadianpour <mohsenhadianpour@gmail.com>
2019-10-05 09:44:09 +0330

    Add getListOfLastFileDates function

--- src/main/scala/ir/son/ix/commons/CSVReader.scala
+++ src/main/scala/ir/son/ix/commons/CSVReader.scala
@@ -12,6 +12,22 @@ object CSVReader {
       .map(_.toString).toList.sorted
   }
 
+  def getListOfLastFileDates(dir: String, date: Int): Int = {
+    val file = new File(dir)
+    val files = file.listFiles.filter(_.isFile)
+      .filter(_.getName.endsWith(".csv"))
+      .map(_.toString).toList.sorted
+
+    val dates = files.map(x => {
+      val y = x.replaceAll("/", "__")
+      val ind = y.lastIndexOf("__")
+      y.slice(ind +2, ind + 10).toInt
+    })
+
+    dates.filter(_ <= date).sorted(Ordering.Int.reverse).head
+
+  }
+
 //  getListOfFiles("dirrectory path")
 
 
